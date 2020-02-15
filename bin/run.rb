43eda849d45d6ffab46ee42f76edee82f4873c82
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-15 18:37:20 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
+require_relative '../config/environment'
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
+
+#require_relative '../config/environment'
+#require 'bundler/setup'
+#Bundler.require(:default, :development)
+#require_relative '../config/environment
